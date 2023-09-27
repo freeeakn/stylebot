b3d21b2311a5856b07f92c63ae0111d84ee78591
freeeakn <1rycont@gmail.com>
2023-09-27 21:03:02 +0700

    rebase db from lite sql to postgresql

--- src/backend/styleSite/siteApp/models.py
+++ src/backend/styleSite/siteApp/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    count = models.IntegerField(primary_key=True, default=1)
+    title = models.CharField(max_length=255)
+    field_name = models.ImageField(upload_to ='uploads/')
+    desc = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, blank=True)
+    price = models.DecimalField()
