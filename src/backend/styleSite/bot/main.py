bd68e0f9424e26d480f39f5dda7afb171ec3e130
andreyakakudryash <kozyuraandreu@gmail.com>
2023-11-03 02:07:38 +0700

    get_weather

--- src/backend/styleSite/bot/main.py
+++ src/backend/styleSite/bot/main.py
@@ -1,6 +1,9 @@
 # libraries
 
 import asyncio
+import os
+import requests
+import json
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import (
     Message,
@@ -15,8 +18,7 @@ from aiogram.types import (
     # SuccessfulPayment,
     InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery,
 )
-import os
-import requests
+
 
 # ----------------------------------------------------------------
 
@@ -31,6 +33,7 @@ from messages import MESSAGES
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot=bot)
+API = 'ff5562c34391a3fce2f7436a78c1661b'
 
 # command list setup
 async def on_startup():
@@ -50,25 +53,39 @@ async def on_startup():
 async def process_start_command(message: Message):
     await message.answer(text=MESSAGES['start'], parse_mode='html')
 
-@dp.message(F.text == '/start')
+#----------------------------------------------------------------
+
+@dp.message(F.text == '/support')
 async def process_start_command(message: Message):
-    await message.answer(text=MESSAGES['start'], parse_mode='html')
+    await message.answer(text=MESSAGES['help'], parse_mode='html')
 
 
+
+@dp.message(F.text == '/get_recommendations')
+async def get_rec_command(message: Message):
+     await message.answer(text=MESSAGES['get_recommendations'], parse_mode='html')
+     await message.answer(text = "Введите ваш город")
+
+       
+@dp.message(F.text)
+async def get_city (message: Message):
+    city = message.text.strip().lower()
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    if data["cod"] == 200:
+        await message.answer(text = f'Сейчас погода: {data["main"]["temp"]}, но нахуй не нужна нам тут эта погода, мы просто проверяем работает эта шайтан машина или нет')
+    else:
+        await message.answer(text = "Извините, возникла ошибка, скорее всего вы неверно указали название города. Попробуйте ещё раз!")
+    # print(data)
+
+    
 
 @dp.message(F.text)
 async def echo(message: Message):
     print(message.text)
     await message.answer(message.text, parse_mode='html')
 
-#----------------------------------------------------------------
-
-@dp.message(F.text == '/get_recommendations')
-async def get_rec_command(message: Message):
-     await message.answer(text=MESSAGES['get_recommendations'], parse_mode='html')
-
-if get_rec_command():
-     print("Введите ваш город: ")
 
 async def main():
     await dp.start_polling(bot)
@@ -80,4 +97,3 @@ if __name__ == '__main__':
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Bot off...")
-
