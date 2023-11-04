ba0a4e8ae3f8fbafe60c2133d74d0b3e5078135f
andreyakakudryash <kozyuraandreu@gmail.com>
2023-11-04 19:59:35 +0700

    add colour to weather

--- src/styleSite/bot/main.py
+++ src/styleSite/bot/main.py
@@ -1,10 +1,13 @@
 # libraries
 
 import asyncio
-import os
+import logging
 import requests
 import json
 from aiogram import Bot, Dispatcher, F
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
 from aiogram.types import (
     Message,
     BotCommand,
@@ -49,6 +52,7 @@ async def on_startup():
 
 # bot functionality
 
+
 @dp.message(F.text == '/start')
 async def process_start_command(message: Message):
     await message.answer(text=MESSAGES['start'], parse_mode='html')
@@ -61,30 +65,72 @@ async def process_start_command(message: Message):
 
 
 
+class Form(StatesGroup):
+    weather = State()
+    colour = State()
+
 @dp.message(F.text == '/get_recommendations')
-async def get_rec_command(message: Message):
+async def get_rec_command(message: Message, state: FSMContext):
+     await state.set_state(Form.weather)
      await message.answer(text=MESSAGES['get_recommendations'], parse_mode='html')
      await message.answer(text = "Введите ваш город")
 
+
+@dp.message(F.text.casefold() == "отмена")
+async def cancel_message(message: Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    logging.info("Cancelling state %r", current_state)
+    await state.clear()
+    await message.reply("Конечно, будем рады помочь вам в следующий раз!")
+
+
        
-@dp.message(F.text)
-async def get_city (message: Message):
+@dp.message(Form.weather)
+async def get_city (message: Message, state: FSMContext):
     city = message.text.strip().lower()
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric'
     response = requests.get(url)
     data = json.loads(response.text)
+    if int(data["main"]["temp"])<=0 and int(data["main"]["temp"])>=-50:
+        print("холодно")
+    if int(data["main"]["temp"])>0 and int(data["main"]["temp"])<=20:
+        print("тепло")
+    if int(data["main"]["temp"])>20 and int(data["main"]["temp"])<40:
+        print("жарко")
+    await state.update_data(weather = message.text)
+    await state.set_state(Form.colour)
     if data["cod"] == 200:
-        await message.answer(text = f'Сейчас погода: {data["main"]["temp"]}, но нахуй не нужна нам тут эта погода, мы просто проверяем работает эта шайтан машина или нет')
+        await message.answer(text = "Отлично, а теперь выберите, какую цветовую гамму одежды вы предпочитаете:",
+    reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+            [
+                KeyboardButton(text="Нейтральные цвета"),
+            ],
+            [
+                KeyboardButton(text="Яркие цвета"),
+            ],
+            [
+                KeyboardButton(text="Контрастные цвета"),
+            ]
+        ],
+            resize_keyboard=True,
+        ),
+    )
     else:
         await message.answer(text = "Извините, возникла ошибка, скорее всего вы неверно указали название города. Попробуйте ещё раз!")
-    # print(data)
 
-    
 
-@dp.message(F.text)
-async def echo(message: Message):
-    print(message.text)
-    await message.answer(message.text, parse_mode='html')
+
+
+
+
+
+
+
+
+
 
 
 async def main():
